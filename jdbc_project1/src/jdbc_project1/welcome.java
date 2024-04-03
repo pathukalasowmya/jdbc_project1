3d7d7be10d8ae80c8c64ba4ea50c8c617ec51b14
pathukalasowmya <165898751+pathukalasowmya@users.noreply.github.com>
2024-04-03 18:15:30 +0530

    Update welcome.java

--- jdbc_project1/src/jdbc_project1/welcome.java
+++ jdbc_project1/src/jdbc_project1/welcome.java
@@ -3,7 +3,7 @@ package jdbc_project1;
 public class welcome {
 	public static void main(String[] args) {
 		System.out.println("welcome to sowmya");
-		
+		System.out.println("welcome to java");
 	}
 
 }
