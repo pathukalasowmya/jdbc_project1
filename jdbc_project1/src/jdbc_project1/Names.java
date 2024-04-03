3431df85b9bac68e313aefac17c1ea285f993e1f
pathukalasowmya <pathukalasowmya@gmail.com>
2024-04-03 18:19:55 +0530

    adding names

---
+++ jdbc_project1/src/jdbc_project1/Names.java
@@ -0,0 +1,9 @@
+package jdbc_project1;
+
+public class Names {
+public static void main(String[] args) {
+	System.out.println("name is nani");
+	System.out.println("name is rebel star");
+	System.out.println("name is surya");
+}
+}
